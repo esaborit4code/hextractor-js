f3a549c0363782019f21a3249cd0fbeca70bdf6a
esaborit4code <saborit.eduardo@gmail.com>
2013-10-27 13:47:19 +0100

    Renamed get_all_matches to get_all_colors and started to use the method .exec_all from RegExp.

--- hextractor.js
+++ hextractor.js
@@ -2,22 +2,16 @@ var clog = console.log;
 jQuery(function($) {
   var HEX_COLOR_REGEXP = /#([0-9a-f]{3,6})/;
   
-  var get_all_matches = function(string, regex) {
-    var raw_match = regex.exec(string);
-    if(!raw_match) { return [] }
-    
-    var first_match = raw_match[0];
-    var the_rest_of_the_string = string.replace(first_match, '');
-    var rest_of_matches = get_all_matches(the_rest_of_the_string, regex);
-    
-    return [first_match].concat(rest_of_matches);      
+  var get_all_colors = function(string) {
+    var all_raw_matches = HEX_COLOR_REGEXP.exec_all(string);
+    return all_raw_matches.map(function(match) { return match[0]; });
   };
   
   var hextract = function(string) {
-    var matches = get_all_matches(string, HEX_COLOR_REGEXP);
-    var lower_case_matches = matches.map(function(color) { return color.toLowerCase(); })
-    var unique_matches = matches.uniq();
-    return ordered_matches = unique_matches.sort();
+    var colors = get_all_colors(string);
+    var lower_case_colors = colors.map(function(color) { return color.toLowerCase(); })
+    var unique_colors = lower_case_colors.uniq();
+    return ordered_colors = unique_colors.sort();
   };
   
   var append_box_for_color = function(color) {
